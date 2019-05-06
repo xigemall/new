b7e782a8193694e6b3bbac5e4689ec90a67c3c10
liuyong <1032380143@qq.com>
2019-05-06 15:25:13 +0800

    网站模型添加logo、ico的IP地址

--- app/Models/Site.php
+++ app/Models/Site.php
@@ -42,4 +42,14 @@ class Site extends Model
     {
         $this->hasMany(SiteNavigationArticle::class);
     }
+
+    public function getLogoAttribute($value)
+    {
+        return config('app.url') . '/' . $value;
+    }
+
+    public function getIcoAttribute($value)
+    {
+        return config('app.url') . '/' . $value;
+    }
 }
