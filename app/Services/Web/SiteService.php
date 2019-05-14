b9c909f5def82c83861794f1e830ec29b7e5bc8a
liuyong <1032380143@qq.com>
2019-05-14 11:51:55 +0800

    api获取文章view_count 默认值为0

--- app/Services/Web/SiteService.php
+++ app/Services/Web/SiteService.php
@@ -9,163 +9,16 @@ use App\Models\AdvertisingSite;
 use App\Models\Article;
 use App\Models\Blogroll;
 use App\Models\BlogrollSite;
-use App\Models\Navigation;
-use App\Models\Site;
-use App\Services\Admin\TemplateService;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class SiteService
 {
-    protected $template;
-
-    public function __construct(TemplateService $templateService)
-    {
-        $this->template = $templateService;
-    }
-
-    /**
-     * 检测网站是否有静态文件
-     * @param $site
-     * @return bool
-     */
-    public function check($site)
-    {
-        $path = public_path('static/' . $site->id);
-        if (is_dir($path)) {
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * 删除网站静态文件、balde文件
-     * @param int $siteId
-     */
-    public function removeStaticFile(int $siteId)
-    {
-        Storage::disk('static')->deleteDirectory($siteId);
-        Storage::disk('resource')->deleteDirectory($siteId);
-    }
-
-    /**
-     *设置静态文件
-     * @param $site
-     */
-    public function set($site)
-    {
-        // 获取广告
-        $advertising = $this->getAdvertising($site->id);
-        $site->advertisings = $advertising;
-
-        // 获取友情链接
-        $blogroll = $this->getBlogroll($site->id);
-        $site->blogrolls = $blogroll;
-
-        //创建blade文件地址
-        $viewPath = $this->makeBladeFolder($site->id);
-        $templatePath = $site->template->file;
-        $files = $this->template->getAllTemplateFile($templatePath);
-        foreach ($files as $k => $v) {
-            $file = str_replace('uploads/', '', $templatePath);
-            $oldName = str_replace($file . '/', '', $v);
-            $name = str_replace('.html', '', $oldName);
-
-            if (strstr($name, '-detail')) {
-                // 详情
-                $this->makeDetailArticle($site, $name, $viewPath, $v);
-            } else {
-                //列表
-                $this->makeList($name, $v, $viewPath, $site);
-
-            }
-        }
-    }
-
-    /**
-     * 列表文件处理
-     * @param $name
-     * @param $file
-     * @param $viewPath
-     * @param $site
-     */
-    protected function makeList($name, $file, $viewPath, $site)
-    {
-        $newName = $name . '.blade.php';
-        copy(public_path('uploads/' . $file), $viewPath . '/' . $newName);
-
-        // 获取栏目文章
-        $navigationArticles = $this->getNavigationArticles($name, $site->id);
-        $site->navigationArticles = $navigationArticles;
-
-        //解析文件
-        $view = view($site->id . '.' . $name, ['site' => $site,]);
-        $html = response($view)->getContent();
-
-        $publicPath = public_path('static/' . $site->id);
-        if (!is_dir(public_path('static'))) {
-            mkdir(public_path('static'), 0777);
-        }
-        if (!is_dir($publicPath)) {
-            mkdir($publicPath, 0777);
-        }
-        file_put_contents($publicPath . '/' . $name . '.html', $html);
-    }
-
-    /**
-     * 详情文件处理
-     * @param $site
-     * @param $name
-     * @param $viewPath
-     * @param $file
-     */
-    protected function makeDetailArticle($site, $name, $viewPath, $file)
-    {
-        $name = str_replace('-detail', '', $name);
-        $viewPath = $this->makeBladeFolder($site->id . '/' . $name);
-
-        // 获取栏目文章
-        $navigationArticles = $this->getNavigationArticles($name, $site->id);
-        if ($navigationArticles) {
-            foreach ($navigationArticles as $v) {
-                copy(public_path('uploads/' . $file), $viewPath . '/' . $v->id . '.blade.php');
-
-                //解析文件
-                $view = view($site->id . '.' . $name . '.' . $v->id, ['site' => $site, 'detail' => $v]);
-                $html = response($view)->getContent();
-
-                $publicPath = public_path('static/' . $site->id . '/' . $name);
-                if (!is_dir(public_path('static'))) {
-                    mkdir(public_path('static'), 0777);
-                }
-                if (!is_dir($publicPath)) {
-                    mkdir($publicPath, 0777);
-                }
-                file_put_contents($publicPath . '/' . $v->id . '.html', $html);
-            }
-        }
-    }
-
-    /**
-     * 创建文件夹
-     * @param $name
-     * @return string
-     */
-    protected function makeBladeFolder($name)
-    {
-        $folder = resource_path('views/' . $name);
-        if (!is_dir($folder)) {
-            mkdir($folder, 0777);
-        }
-        return $folder;
-    }
 
     /**
      * 获取广告
      * @param $siteId
      * @return mixed
      */
-    protected function getAdvertising($siteId)
+    public function getAdvertising($siteId)
     {
         // 网站广告ID
         $advertisingId = AdvertisingSite::where('site_id', $siteId)->pluck('advertising_id')->all();
@@ -179,7 +32,7 @@ class SiteService
      * @param $siteId
      * @return mixed
      */
-    protected function getBlogroll($siteId)
+    public function getBlogroll($siteId)
     {
         $blogrollId = BlogrollSite::where('site_id', $siteId)->pluck('blogroll_id')->all();
         $blogroll = Blogroll::where('place', 0)->orWhereIn('id', $blogrollId)->get();
@@ -188,17 +41,37 @@ class SiteService
 
     /**
      * 获取栏目文章
-     * @param string $navigationName
+     * @param int $navigationId
      * @param int $siteId
      * @return mixed
      */
-    protected function getNavigationArticles(string $navigationName, int $siteId)
+    public function getNavigationArticles(int $navigationId, int $siteId)
     {
-        $navigationArticles = [];
-        $navigationId = Navigation::where(['site_id' => $siteId, 'name' => $navigationName])->value('id');
-        if ($navigationId) {
-            $navigationArticles = Article::where(['site_id' => $siteId, 'navigation_id' => $navigationId])->get();
-        }
+        $navigationArticles = Article::where(['site_id' => $siteId, 'navigation_id' => $navigationId])->paginate(10);
         return $navigationArticles;
+    }
+
+    /**
+     * 获取推荐文章
+     * @param $siteId
+     * @param $navigationId
+     * @return mixed
+     */
+    public function getRecommendArticles($siteId, $navigationId)
+    {
+        $articles = Article::where(['site_id' => $siteId, 'navigation_id' => $navigationId])->inRandomOrder()->limit(5)->get();
+        return $articles;
+    }
+
+    /**
+     * 获取热门文章
+     * @param $siteId
+     * @param $navigationId
+     * @return mixed
+     */
+    public function getHotArticles($siteId, $navigationId)
+    {
+        $articles = Article::where(['site_id' => $siteId, 'navigation_id' => $navigationId])->orderBy('view_count', 'desc')->limit(5)->get();
+        return $articles;
     }
 }
