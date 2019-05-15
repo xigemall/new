9108bd361f8eced1f9f3b84bacc581359cf6491a
liuyong <1032380143@qq.com>
2019-05-15 14:57:23 +0800

    标签表字段优化

--- database/migrations/2019_05_14_202347_create_tags_table.php
+++ database/migrations/2019_05_14_202347_create_tags_table.php
@@ -15,8 +15,6 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('site_id')->comment('网站ID')->index();
-            $table->unsignedInteger('navigation_id')->comment('栏目ID')->index();
             $table->unsignedInteger('article_id')->comment('文章ID')->index();
             $table->char('name','50')->comment('标签名称')->default('');
             $table->timestamps();
